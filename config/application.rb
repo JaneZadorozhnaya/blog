030f9b139ed4f2baeffb68cb62379c40245c408c
Evgenia Zadorozhnaya <jane.zev@gmail.com>
2014-08-15 12:43:38 +0300

    For Heroku

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -20,5 +20,6 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #config.serve_static_assets = true
   end
 end
