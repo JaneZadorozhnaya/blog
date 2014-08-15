43abbb20a9cdb018e4ecf6ccd87f9a9634899ea5
Evgenia Zadorozhnaya <jane.zev@gmail.com>
2014-08-15 13:23:34 +0300

    changed production for email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Blog::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = { :host => 'intense-lowlands-1465.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
